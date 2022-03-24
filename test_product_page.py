bc48b7f0b4196e61cfee74db1e017b75af58585e
Sergei <sergesustavov@yandex.ru>
2022-03-24 20:20:32 +0300

    Added parameters to product page tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,12 @@
+import pytest
 from pages.product_page import ProductPage
 
-
-def test_guest_can_add_product_to_basket(browser):
-    url = 'http://selenium1py.pythonanywhere.com/es/catalogue/the-shellcoders-handbook_209/?promo=newYear'
-    product_page = ProductPage(browser, url)
+@pytest.mark.parametrize('link', [0, 1, 2, 3, 4, 5, 6,
+                                  pytest.param(7, marks=pytest.mark.xfail),
+                                  8, 9])
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}'
+    product_page = ProductPage(browser, link)
     product_page.open()
     product_page.add_to_cart()
     product_page.solve_quiz_and_get_code()
